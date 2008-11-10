9e7a75facab91a68087699005920acd7c32033f9
Brandon Dimcheff <bdimchef-git@wieldim.com>
2008-11-10 10:47:17 -0500

    wtf

--- lib/rag/aggregate.rb
+++ lib/rag/aggregate.rb
@@ -19,39 +19,65 @@ module Rag
       end
     end
     
-    attr_accessor :input_data, :groups
+    attr_accessor :input_data, :aggregates
     private :input_data=
     
     def initialize(input_data)
       self.input_data = input_data
-      self.groups = {}
+      self.aggregates = {}
     end
     
     def aggregate
-      parse_input
+      # look up block for each group and aggregate
       
-      groups.map do |group, group_values|
-        group_results = self.class.aggregators.map do |aggregator|
-          column = group_values.map { |row| row[aggregator.column] }  # TODO this is slow
-          aggregator.block.call column
-        end
-        group + group_results
-      end
-    end
-    
-    private
-    def parse_input
-      grouping = self.class.grouping || []
-
-      input_data.map do |input_row|
+      input_data.each do |input_row|
         input_array = input_row.split(',').map {|c| c.strip } # TODO put this somewhere else, support more formats
         input_array.extend(Rag::Array)
+        row_group = input_array.extract_columns(*(self.class.grouping))
         
-        row_group = input_array.extract_columns(*grouping)
-
-        group_data = self.groups[row_group] ||= []
-        group_data << input_array
+        self.class.aggregators.each do |aggregator|
+          self.aggregates[[row_group, aggregator]] ||= aggregator.start
+          
+          aggregator.block.call(input_array[aggregator.column])
+          
+          # aggregator_block = self.aggregates[[row_group, aggregator]] ||= lambda do |acc, row|
+          #   # inject into aggregator block
+          #   memo = aggregator.start
+          #   aggregator.block.call(acc, row)
+          # end
+          
+          aggregator_block.call(input_array[aggregator.column])
+        end
       end
+      
+      
     end
+    
+    # def aggregate
+    #   parse_input
+    #   
+    #   groups.map do |group, group_values|
+    #     group_results = self.class.aggregators.map do |aggregator|
+    #       column = group_values.map { |row| row[aggregator.column] }  # TODO this is slow
+    #       aggregator.block.call column
+    #     end
+    #     group + group_results
+    #   end
+    # end
+    # 
+    # private
+    # def parse_input
+    #   grouping = self.class.grouping || []
+    # 
+    #   input_data.map do |input_row|
+    #     input_array = input_row.split(',').map {|c| c.strip } # TODO put this somewhere else, support more formats
+    #     input_array.extend(Rag::Array)
+    #     
+    #     row_group = input_array.extract_columns(*grouping)
+    # 
+    #     group_data = self.groups[row_group] ||= []
+    #     group_data << input_array
+    #   end
+    # end
   end
 end
--- lib/rag/aggregator.rb
+++ lib/rag/aggregator.rb
@@ -1,27 +1,24 @@
 module Rag
-  class Aggregator
-    include Enumerable
-    
-    attr_accessor :column, :block
+  class Aggregator    
+    attr_accessor :column, :start, :block
     
     def initialize(column)
       self.column = column
     end
     
-    def all(&block)
+    # def all(&block)
+    #   self.block = block
+    # end
+    
+    def inject(start, &block)
+      self.start = start
       self.block = block
     end
     
-    def inject(start, &block)
-      self.all do |data|
-        data.inject(start, &block)
-      end
-    end
-    
-    def sum
-      self.all do |data|
-        data.inject(0) { |acc,i| acc + i.to_i }
-      end
-    end
+    # def sum
+    #   self.all do |data|
+    #     data.inject(0) { |acc,i| acc + i.to_i }
+    #   end
+    # end
   end
 end
